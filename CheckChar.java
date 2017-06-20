7431044bd15bc5f456fb42efaa47cb4ed5078654
Harini2308 <harini.pinky23@gmail.com>
2017-06-20 16:36:15 +0530

    Create CheckChar.java

---
+++ CheckChar.java
@@ -0,0 +1,22 @@
+package myfrstjavaproj;
+import java.util.*;
+public class CheckChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner c=new Scanner(System.in);
+		System.out.println("enter a char to check if it is a vowel or consonant:");
+		char t;
+		t=c.next().charAt(0);
+		if(t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u' || t == 'A' || t == 'E' || t == 'I' || t == 'O' || t == 'U')
+		{
+			System.out.println("Given character is a vowel!!");
+			
+		}
+		else
+		{
+			System.out.println("Given character is a consonant!!");
+		}
+	}
+
+}
