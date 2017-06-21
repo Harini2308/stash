ce4509f54a5046912739da51bc8bf68decd0c9a5
Harini2308 <harini.pinky23@gmail.com>
2017-06-21 09:36:30 +0530

    Update CheckChar.java

--- CheckChar.java
+++ CheckChar.java
@@ -8,14 +8,20 @@ public class CheckChar {
 		System.out.println("enter a char to check if it is a vowel or consonant:");
 		char t;
 		t=c.next().charAt(0);
-		if(t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u' || t == 'A' || t == 'E' || t == 'I' || t == 'O' || t == 'U')
+		if(Character.isLetter(t))
 		{
-			System.out.println("Given character is a vowel!!");
-			
+			if(t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u' || t == 'A' || t == 'E' || t == 'I' || t == 'O' || t == 'U')
+			{
+				System.out.println("Given character is a vowel!!");
+			}
+			else
+			{
+				System.out.println("Given character is a consonant!!");
+			}
 		}
 		else
 		{
-			System.out.println("Given character is a consonant!!");
+			System.out.println("Enter  alphabet!Given input is not a alphabet!");
 		}
 	}
 
