d0308026a8bf7f97b2380f8139177fe42865c9d1
Harini2308 <harini.pinky23@gmail.com>
2017-06-23 10:13:45 +0530

    Update ReverseDigit.java

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -8,11 +8,18 @@ public class ReverseDigits {
 		System.out.println("Enter a number");
 		int no=t.nextInt();
 		int rem=0,rev=0;
+		try
+		{
 		while(no!=0)
 		{
 		rem=no%10;
 		rev=rev*10+rem;
 		no=no/10;
+		}
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("not a number!);
 		}
 		System.out.println(rev);
 	}
